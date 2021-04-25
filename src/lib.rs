a25951663e6cdb54cb619faf3cc8e708206452ce
Jayce Fayne <jayce.fayne@mailbox.org>
2021-04-25 19:17:47 +0200

    simplify

--- src/lib.rs
+++ src/lib.rs
@@ -16,17 +16,18 @@ type CowString = Cow<'static, str>;
 
 #[derive(Debug)]
 pub struct Button {
-    button_type: &'static str,
+    button_type: CowString,
     text: CowString,
     action: Option<CowString>,
 }
 
 impl Button {
     fn new(
-        button_type: &'static str,
+        button_type: impl Into<CowString>,
         text: impl Into<CowString>,
         action: Option<CowString>,
     ) -> Self {
+        let button_type = button_type.into();
         let text = text.into();
         Self {
             button_type,
@@ -81,186 +82,145 @@ pub struct Swaynag {
     detailed_message: Option<CowString>,
     terminal: Option<CowString>,
     args: Vec<CowString>,
-    buttons: Vec<Button>,
 }
 
 impl Swaynag {
     pub fn new(message: impl Into<CowString>) -> Self {
         Self {
-            args: Vec::new(),
-            buttons: Vec::new(),
             message: message.into(),
-            terminal: None,
             detailed_message: None,
+            terminal: None,
+            args: Vec::new(),
         }
     }
 
-    pub fn terminal(mut self, terminal: impl Into<CowString>) -> Self {
+    pub fn terminal(&mut self, terminal: impl Into<CowString>) -> &mut Self {
         self.terminal = Some(terminal.into());
         self
     }
 
-    pub fn detailed_message(mut self, detailed_message: impl Into<CowString>) -> Self {
+    pub fn detailed_message(&mut self, detailed_message: impl Into<CowString>) -> &mut Self {
         self.detailed_message = Some(detailed_message.into());
         self
     }
 
-    fn arg(&mut self, arg: impl Into<CowString>) {
+    fn arg(&mut self, arg: impl Into<CowString>) -> &mut Self {
         self.args.push(arg.into());
-    }
-
-    pub fn edge(mut self, edge: Edge) -> Self {
-        self.arg("-e");
-        self.arg(edge);
         self
     }
 
-    pub fn font(mut self, font: impl Into<CowString>) -> Self {
-        self.arg("-f");
-        self.arg(font);
+    pub fn edge(&mut self, edge: Edge) -> &mut Self {
+        self.arg("-e").arg(edge)
+    }
+
+    pub fn font(&mut self, font: impl Into<CowString>) -> &mut Self {
+        self.arg("-f").arg(font)
+    }
+
+    pub fn message_type(&mut self, message_type: impl Into<CowString>) -> &mut Self {
+        self.arg("-t").arg(message_type)
+    }
+
+    pub fn output(&mut self, output: impl Into<CowString>) -> &mut Self {
+        self.arg("-o").arg(output)
+    }
+
+    pub fn background(&mut self, background: impl Into<CowString>) -> &mut Self {
+        self.arg("--background").arg(background)
+    }
+
+    pub fn border(&mut self, border: impl Into<CowString>) -> &mut Self {
+        self.arg("--border").arg(border)
+    }
+
+    pub fn border_bottom(&mut self, border_bottom: impl Into<CowString>) -> &mut Self {
+        self.arg("--border-bottom").arg(border_bottom)
+    }
+
+    pub fn button_background(&mut self, button_background: impl Into<CowString>) -> &mut Self {
+        self.arg("--button-background").arg(button_background)
+    }
+
+    pub fn text(&mut self, text: impl Into<CowString>) -> &mut Self {
+        self.arg("--text").arg(text)
+    }
+
+    pub fn button_text(&mut self, button_text: impl Into<CowString>) -> &mut Self {
+        self.arg("--button-text").arg(button_text)
+    }
+
+    pub fn border_bottom_size(&mut self, border_bottom_size: impl Into<CowString>) -> &mut Self {
+        self.arg("--border-bottom-size").arg(border_bottom_size)
+    }
+
+    pub fn message_padding(&mut self, message_padding: impl Into<CowString>) -> &mut Self {
+        self.arg("--message-padding").arg(message_padding)
+    }
+
+    pub fn details_background(&mut self, details_background: impl Into<CowString>) -> &mut Self {
+        self.arg("--details-background").arg(details_background)
+    }
+
+    pub fn details_border_size(&mut self, details_border_size: impl Into<CowString>) -> &mut Self {
+        self.arg("--details-border-size").arg(details_border_size)
+    }
+
+    pub fn button_border_size(&mut self, button_border_size: impl Into<CowString>) -> &mut Self {
+        self.arg("--button-border-size").arg(button_border_size)
+    }
+
+    pub fn button_gap(&mut self, button_gap: impl Into<CowString>) -> &mut Self {
+        self.arg("--button-gap").arg(button_gap)
+    }
+
+    pub fn button_dismiss_gap(&mut self, button_dismiss_gap: impl Into<CowString>) -> &mut Self {
+        self.arg("--button-dismiss-gap").arg(button_dismiss_gap)
+    }
+
+    pub fn button_margin_right(&mut self, button_margin_right: impl Into<CowString>) -> &mut Self {
+        self.arg("--button-margin-right").arg(button_margin_right)
+    }
+
+    pub fn button_padding(&mut self, button_padding: impl Into<CowString>) -> &mut Self {
+        self.arg("--button-padding").arg(button_padding)
+    }
+
+    pub fn button(
+        &mut self,
+        Button {
+            button_type,
+            text,
+            action,
+        }: Button,
+    ) -> &mut Self {
+        self.args.push(button_type);
+        self.args.push(text);
+        if let Some(action) = action {
+            self.args.push(action);
+        }
         self
     }
 
-    pub fn message_type(mut self, message_type: impl Into<CowString>) -> Self {
-        self.arg("-t");
-        self.arg(message_type);
-        self
-    }
-
-    pub fn output(mut self, output: impl Into<CowString>) -> Self {
-        self.arg("-o");
-        self.arg(output);
-        self
-    }
-
-    pub fn background(mut self, background: impl Into<CowString>) -> Self {
-        self.arg("--background");
-        self.arg(background);
-        self
-    }
-
-    pub fn border(mut self, border: impl Into<CowString>) -> Self {
-        self.arg("--border");
-        self.arg(border);
-        self
-    }
-
-    pub fn border_bottom(mut self, border_bottom: impl Into<CowString>) -> Self {
-        self.arg("--border-bottom");
-        self.arg(border_bottom);
-        self
-    }
-
-    pub fn button_background(mut self, button_background: impl Into<CowString>) -> Self {
-        self.arg("--button-background");
-        self.arg(button_background);
-        self
-    }
-
-    pub fn text(mut self, text: impl Into<CowString>) -> Self {
-        self.arg("--text");
-        self.arg(text);
-        self
-    }
-
-    pub fn button_text(mut self, button_text: impl Into<CowString>) -> Self {
-        self.arg("--button-text");
-        self.arg(button_text);
-        self
-    }
-
-    pub fn border_bottom_size(mut self, border_bottom_size: impl Into<CowString>) -> Self {
-        self.arg("--border-bottom-size");
-        self.arg(border_bottom_size);
-        self
-    }
-
-    pub fn message_padding(mut self, message_padding: impl Into<CowString>) -> Self {
-        self.arg("--message-padding");
-        self.arg(message_padding);
-        self
-    }
-
-    pub fn details_background(mut self, details_background: impl Into<CowString>) -> Self {
-        self.arg("--details-background");
-        self.arg(details_background);
-        self
-    }
-
-    pub fn details_border_size(mut self, details_border_size: impl Into<CowString>) -> Self {
-        self.arg("--details-border-siz");
-        self.arg(details_border_size);
-        self
-    }
-
-    pub fn button_border_size(mut self, button_border_size: impl Into<CowString>) -> Self {
-        self.arg("--button-border-size");
-        self.arg(button_border_size);
-        self
-    }
-
-    pub fn button_gap(mut self, button_gap: impl Into<CowString>) -> Self {
-        self.arg("--button-gap");
-        self.arg(button_gap);
-        self
-    }
-
-    pub fn button_dismiss_gap(mut self, button_dismiss_gap: impl Into<CowString>) -> Self {
-        self.arg("--button-dismiss-gap");
-        self.arg(button_dismiss_gap);
-        self
-    }
-
-    pub fn button_margin_right(mut self, button_margin_right: impl Into<CowString>) -> Self {
-        self.arg("--button-margin-right");
-        self.arg(button_margin_right);
-        self
-    }
-
-    pub fn button_padding(mut self, button_padding: impl Into<CowString>) -> Self {
-        self.arg("--button-padding");
-        self.arg(button_padding);
-        self
-    }
-
-    pub fn button(mut self, button: Button) -> Self {
-        self.buttons.push(button);
-        self
-    }
-
-    pub fn buttons(mut self, buttons: impl IntoIterator<Item = Button>) -> Self {
+    pub fn buttons(&mut self, buttons: impl IntoIterator<Item = Button>) -> &mut Self {
         for button in buttons {
-            self.buttons.push(button);
+            self.button(button);
         }
         self
     }
 
     fn spawn_child(&self) -> Result<Child> {
         let mut cmd = Command::new("swaynag");
-        cmd.stdout(Stdio::null());
-        cmd.stderr(Stdio::null());
-        cmd.arg("-m");
-        cmd.arg(self.message.as_ref());
         if let Some(ref terminal) = self.terminal {
             cmd.env("TERMINAL", terminal.as_ref());
         }
-        cmd.args(self.args.iter().map(|e| e.as_ref()));
-        for Button {
-            button_type,
-            text,
-            action,
-        } in self.buttons.iter()
-        {
-            cmd.arg(button_type);
-            cmd.arg(text.as_ref());
-            if let Some(action) = action {
-                cmd.arg(action.as_ref());
-            }
-        }
         if self.detailed_message.is_some() {
-            cmd.arg("-l");
-            cmd.stdin(Stdio::piped());
+            cmd.arg("-l").stdin(Stdio::piped());
         }
-        cmd.spawn()
+        cmd.stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .arg("-m")
+            .arg(self.message.as_ref())
+            .args(self.args.iter().map(|a| a.as_ref()))
+            .spawn()
     }
 }
